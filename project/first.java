3cdafde370f5e8b8e74906f4a7d50d1af61e8ae6
Iamneo <techadmin@iamneo.ai>
2023-08-30 11:52:39 +0000

    Backup Commit

--- project/first.java
+++ project/first.java
@@ -1,12 +1,18 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class first{
     public static void main(String[] args) {
         List<Integer> lisname = new ArrayList<>();       
         lisname.add(1);
+        lisname.add(2);
+        lisname.add(3);
+        lisname.add(4);
         lisname.add(1);
-        lisname.add(1);
-        lisname.add(1);
+        lisname.add(2);
+        lisname.add(3);
+        lisname.add(4);
+        System.out.println(lisname);
     }
 }
