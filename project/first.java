2d80117655a896df24c0e72acdb79a8039fc97a1
Iamneo <techadmin@iamneo.ai>
2023-08-30 11:47:38 +0000

    Backup Commit

---
+++ project/first.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class first{
+    public static void main(String[] args) {
+        List<Integer> lisname = new ArrayList<>();       
+        lisname.add(1);
+        lisname.add(1);
+        lisname.add(1);
+        lisname.add(1);
+    }
+}
