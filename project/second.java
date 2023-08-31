f670598d6db544552b6f4e870a8b59d08080f44e
Iamneo <techadmin@iamneo.ai>
2023-08-31 05:12:46 +0000

    Backup Commit

---
+++ project/second.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.*;
+public class second {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        List<String>bookname = new ArrayList<>();
+        List<Integer>price = new ArrayList<>();
+        System.out.println("enter the book name");
+        bookname.add(s.next());
+        price.a
+
+    
+    }
+}
